e19f434daf4ff7382c38580f9f23b0de8ce197b1
Mehedi001 <mh.hridoy5070@gmail.com>
2023-06-10 16:41:49 +0600

    registrator page edit done

---
+++ src/Pages/Registration/Registration.jsx
@@ -0,0 +1,127 @@
+/* eslint-disable react/no-unknown-property */
+
+import { Link } from 'react-router-dom';
+import './Registration.css'
+import { useState } from 'react';
+
+
+const Registration = () => {
+    const [password, setPassword] = useState();
+    const [confirmPassword, setConfirmPassword] = useState();
+
+    const handlePasswordChange = (event) => {
+        const newPassword = event.target.value;
+        setPassword(newPassword);
+
+
+    }
+    const handleConfirmPassword = (event) => {
+        const newPassword = event.target.value;
+        setConfirmPassword(newPassword);
+
+
+    }
+
+    const handleCheckPassword = () => {
+        if (password === confirmPassword) {
+            console.log('ok')
+        }
+
+        else {
+            console.log("not okay")
+        }
+
+
+    }
+
+    const handleRegister = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const firstName = form.firstName.value;
+        const lastName = form.lastName.value;
+        const email = form.email.value;
+        const role = form.role.value;
+        const password = form.confirmPassword.value;
+        const photo = form.photo.value;
+        const userData = { firstName, lastName, email, role, password, photo, userData }
+    }
+    return (
+        <div style={{ background: " linear-gradient(to bottom, rgba(0,0,0,0.7), rgba(0,0,0,0.7)), url('login-background2.jpg')", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center" }} className=" p-2 lg:p-20 bg-gray-800 h-screen">
+            <h1 className=" text-3xl lg:text-6xl text-center text-[tomato]/50 font-semibold">Register Now</h1>
+            <form onSubmit={handleRegister} className="w-full max-w-lg mx-0 lg:my-10 lg:mx-auto">
+                <div className="flex flex-wrap lg:-mx-3 mb-6">
+                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label className="block uppercase tracking-wide text-orange-100 text-xs font-bold mb-2" >
+                            First Name
+                        </label>
+                        <input className="appearance-none block w-full bg-gray-600 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" name="firstName" placeholder="First Name" required />
+
+                    </div>
+                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label className="block uppercase tracking-wide text-orange-100 text-xs font-bold mb-2">
+                            Last Name
+                        </label>
+                        <input className="appearance-none block w-full bg-gray-600 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" name="lastName" placeholder="Last Name" required />
+
+                    </div>
+                    <div className="w-full px-3">
+                        <label className="block uppercase tracking-wide text-orange-100 text-xs font-bold mb-2">
+                            Email
+                        </label>
+                        <input className="appearance-none block w-full bg-gray-600 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="email" name="email" placeholder="E-mail" required />
+                    </div>
+                </div>
+
+
+                <div className="flex flex-wrap lg:-mx-3 mb-6">
+                    <label className="block mx-3 uppercase tracking-wide text-orange-100 text-xs font-bold mb-2">
+                        Select Your Role
+                    </label>
+                    <div className="w-full relative px-3">
+                        <select name="role" className="block appearance-none w-full bg-gray-600 border border-gray-200 text-orange-100 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:text-gray-100 focus:border-gray-500 " id="grid-state">
+
+                            <option disabled></option>
+                            <option>Student</option>
+                            <option>Instructor</option>
+
+                        </select>
+                        <div className="pointer-events-none absolute -inset-y-2 right-4 flex items-center px-2 text-gray-700">
+                            <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+                        </div>
+                        <p className="text-orange-100 text-xs italic">Select who are you, student or instructor</p>
+                    </div>
+                </div>
+
+                <div className="flex flex-wrap lg:-mx-3 mb-2">
+                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label className="block uppercase tracking-wide text-orange-100 text-xs font-bold mb-2">
+                            Password
+                        </label>
+                        <input onChange={handlePasswordChange} className="appearance-none block w-full bg-gray-600 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="password" name="password" placeholder="Password" required />
+                    </div>
+                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label className="block uppercase tracking-wide text-orange-100 text-xs font-bold mb-2">
+                            Confirm Password
+                        </label>
+                        <input onChange={handleConfirmPassword} onBlur={handleCheckPassword} className="appearance-none block w-full bg-gray-600 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="password" name="confirmPassword" placeholder="Confirm Password" required />
+                    </div>
+                </div>
+                <div className="flex flex-wrap lg:-mx-3 mb-6">
+
+                    <div className="w-full px-3">
+                        <label className="block uppercase tracking-wide text-orange-100 text-xs font-bold mb-2">
+                            Photo URL
+                        </label>
+                        <input className="appearance-none block w-full bg-gray-600 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" name="photo" placeholder="Photo URL" />
+
+                    </div>
+                </div>
+                <p className="text-orange-100 text-sm  my-4">Already Have Account <Link className='underline text-blue-500' to="/login">Login Here</Link> </p>
+                <input className="bg-[tomato]/50 btn w-full border-0 text-white rounded-md px-2 py-1" type="submit" value="Add" />
+            </form>
+
+        </div>
+    );
+};
+
+export default Registration;
