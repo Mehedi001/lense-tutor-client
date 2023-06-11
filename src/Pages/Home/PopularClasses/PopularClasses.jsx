bda7b502e0c4bad4481fc4ec39f006c607f98117
Mehedi001 <mh.hridoy5070@gmail.com>
2023-06-12 04:26:04 +0600

    dynamically load classes data

--- src/Pages/Home/PopularClasses/PopularClasses.jsx
+++ src/Pages/Home/PopularClasses/PopularClasses.jsx
@@ -4,9 +4,18 @@ import "swiper/css";
 import "swiper/css/pagination";
 import { FreeMode, Pagination } from "swiper";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 
 const PopularClasses = () => {
+
+    const [classes, setClasses] = useState([]);
+
+    useEffect (() => {
+        fetch('http://localhost:5000/classes')
+        .then(res => res.json())
+        .then(data => setClasses(data))
+    },[])
     
     return (
         <div className="py-12 bg-gray-800 px-8">
@@ -47,89 +56,23 @@ const PopularClasses = () => {
         modules={[FreeMode, Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide>
-            <div className=" w-96 bg-[#18110e]">
-                <img className="w-full h-64  mx-auto" src="https://images.pexels.com/photos/3921852/pexels-photo-3921852.jpeg" alt="" />
-                <div className="my-2 p-4 text-[#c58f63]">
-                <h1 className=" text-xl">Course Name:  <span className="font-semibold text-2xl block"> Potrait Photography</span> </h1>
-                <p className="font-light my-2">If anyone want to learn potrait photography professionally, <br /> Enroll this professional Class & Admit to this course</p>
-                <p className="font-bold text-[#c0afa3]">Total 200+ <Link className="hover:cursor-pointer hover:underline hover:text-white">Enroll</Link> Course</p>
-                </div>
-            </div>
-        </SwiperSlide>
-        <SwiperSlide>
-            <div className=" w-96 bg-[#18110e]">
-                <img className="w-full h-64  mx-auto" src="https://images.pexels.com/photos/17042036/pexels-photo-17042036/free-photo-of-young-woman-holding-a-film-roll-in-front-of-her-eyes.jpeg" alt="" />
-                <div className="my-2 p-4 text-[#c58f63]">
-                <h1 className=" text-xl">Course Name:  <span className="font-semibold text-2xl block"> Black & White Photography</span> </h1>
-                <p className="font-light my-2">If anyone want to learn black & white photography professionally, <br /> Enroll this professional Class & Admit to this course</p>
-                <p className="font-bold text-[#c0afa3]">Total 250+ <Link className="hover:cursor-pointer hover:underline hover:text-white">Enroll</Link> Course</p>
-                </div>
-            </div>
-        </SwiperSlide>
-        <SwiperSlide>
-            <div className=" w-96 bg-[#18110e]">
-                <img className="w-full h-64  mx-auto" src="https://images.pexels.com/photos/2853432/pexels-photo-2853432.jpeg" alt="" />
-                <div className="my-2 p-4 text-[#c58f63]">
-                <h1 className=" text-xl">Course Name:  <span className="font-semibold text-2xl block"> Landscape Photography</span> </h1>
-                <p className="font-light my-2">If anyone want to learn Landscape Photography professionally, <br /> Enroll this professional Class & Admit to this course</p>
-                <p className="font-bold text-[#c0afa3]">Total 230+ <Link className="hover:cursor-pointer hover:underline hover:text-white">Enroll</Link> Course</p>
-                </div>
-            </div>
-        </SwiperSlide>
-        <SwiperSlide>
-            <div className=" w-96 bg-[#18110e]">
-                <img className="w-full h-64  mx-auto" src="https://images.pexels.com/photos/1344537/pexels-photo-1344537.jpeg" alt="" />
-                <div className="my-2 p-4 text-[#c58f63]">
-                <h1 className=" text-xl">Course Name:  <span className="font-semibold text-2xl block"> Street Photography</span> </h1>
-                <p className="font-light my-2">If anyone want to learn Street Photography professionally, <br /> Enroll this professional Class & Admit to this course</p>
-                <p className="font-bold text-[#c0afa3]">Total 150+ <Link className="hover:cursor-pointer hover:underline hover:text-white">Enroll</Link> Course</p>
-                </div>
-            </div>
-        </SwiperSlide>
-        <SwiperSlide>
-            <div className=" w-96 bg-[#18110e]">
-                <img className="w-full h-64  mx-auto" src="https://images.pexels.com/photos/7610457/pexels-photo-7610457.jpeg" alt="" />
-                <div className="my-2 p-4 text-[#c58f63]">
-                <h1 className=" text-xl">Course Name:  <span className="font-semibold text-2xl block"> Edit Photo</span> </h1>
-                <p className="font-light my-2">If anyone want to learn Edit your Photography professionally, <br /> Enroll this professional Class & Admit to this course</p>
-                <p className="font-bold text-[#c0afa3]">Total 750+ <Link className="hover:cursor-pointer hover:underline hover:text-white">Enroll</Link> Course</p>
-                </div>
-            </div>
-        </SwiperSlide>
-        <SwiperSlide>
-            <div className=" w-96 bg-[#18110e]">
-                <img className="w-full h-64  mx-auto" src="https://images.pexels.com/photos/12811291/pexels-photo-12811291.jpeg" alt="" />
-                <div className="my-2 p-4 text-[#c58f63]">
-                <h1 className=" text-xl">Course Name:  <span className="font-semibold text-2xl block"> Product Photography</span> </h1>
-                <p className="font-light my-2">If anyone want to learn Product Photography professionally, <br /> Enroll this professional Class & Admit to this course</p>
-                <p className="font-bold text-[#c0afa3]">Total 190+ <Link className="hover:cursor-pointer hover:underline hover:text-white">Enroll</Link> Course</p>
-                </div>
-            </div>
-        </SwiperSlide>
-        <SwiperSlide>
-            <div className=" w-96 bg-[#18110e]">
-                <img className="w-full h-64  mx-auto" src="https://images.pexels.com/photos/9856375/pexels-photo-9856375.jpeg" alt="" />
-                <div className="my-2 p-4 text-[#c58f63]">
-                <h1 className=" text-xl">Course Name:  <span className="font-semibold text-2xl block"> Fashion Photography</span> </h1>
-                <p className="font-light my-2">If anyone want to learn fashion Photography professionally, <br /> Enroll this professional Class & Admit to this course</p>
-                <p className="font-bold text-[#c0afa3]">Total 270+ <Link className="hover:cursor-pointer hover:underline hover:text-white">Enroll</Link> Course</p>
-                </div>
-            </div>
-        </SwiperSlide>
-        <SwiperSlide>
-            <div className=" w-96 bg-[#18110e]">
-                <img className="w-full h-64  mx-auto" src="https://images.pexels.com/photos/3584968/pexels-photo-3584968.jpeg" alt="" />
-                <div className="my-2 p-4 text-[#c58f63]">
-                <h1 className=" text-xl">Course Name:  <span className="font-semibold text-2xl block"> Mobile Photography</span> </h1>
-                <p className="font-light my-2">If anyone want to learn Photography professionally with smartphone, <br /> Enroll this professional Class & Admit to this course</p>
-                <p className="font-bold text-[#c0afa3]">Total 500+ <Link className="hover:cursor-pointer hover:underline hover:text-white">Enroll</Link> Course</p>
-                </div>
-            </div>
-        </SwiperSlide>
         
         
         
+   {
+        classes.map (classs => 
+            <SwiperSlide key={classs._id}>
+            <div className=" w-96 bg-[#18110e]">
+                <img className="w-full h-64  mx-auto" src={classs.photo} alt="" />
+                <div className="my-2 p-4 text-[#c58f63]">
+                <h1 className=" text-xl">Course Name:  <span className="font-semibold text-2xl block"> {classs.className}</span> </h1>
+                <p className="font-light my-2">{classs.description}</p>
+                <p className="font-bold text-[#c0afa3]">Available: &nbsp;{classs.availableSeats} &nbsp; Seats</p>
+                </div>
+            </div>
+        </SwiperSlide>
+            )
+   }
         
       </Swiper>
         </div>
