8921e072b28f335245eb1bf02732174bb442429e
mehedi001 <mh.hridoy5070@gmail.com>
2023-10-25 15:38:34 +0600

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
 import './index.css'
-import router from './Routes/Routes.jsx';
+import router from './Routes/Routes';
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './AuthProvider/AuthProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
